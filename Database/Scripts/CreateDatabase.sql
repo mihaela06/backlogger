727363f3dc44e934b2623b6ee25b2f63645346d3
mihaela06 <62171914+mihaela06@users.noreply.github.com>
2020-12-19 12:37:34 +0200

    Inital commit
    Database created and ADO.NET Data Model added

---
+++ Database/Scripts/CreateDatabase.sql
@@ -0,0 +1,37 @@
+﻿USE master
+GO
+
+DECLARE @ProjectPath NVARCHAR(MAX);
+DECLARE @DataPath NVARCHAR(MAX);
+DECLARE @SqlCode NVARCHAR(MAX);
+
+-- Modify this variable to the actual path where you saved the project
+SET @ProjectPath = N'E:\ATM\An III\Semestrul I\Aplicații baze de date\Proiect\Backlogger';
+
+SET @DataPath = @ProjectPath + '\Database\Data\';
+PRINT N'Installing database in ' + @DataPath;
+
+IF EXISTS (SELECT * FROM sys.databases WHERE Name = 'Backlogger')
+	PRINT N'The database already exists';
+	-- DROP DATABASE Backlogger;
+ELSE
+	SET @SqlCode = 'CREATE DATABASE Backlogger
+	ON PRIMARY
+	(
+		Name = BackloggerMasterData,
+		FileName = ''' + @DataPath + 'BackloggerMasterData.mdf''
+	),
+	(
+		Name = BackloggerNonMasterData,
+		FileName = ''' + @DataPath + 'BackloggerNonMasterData.ndf''
+	)
+	LOG ON
+	(
+		Name = BackloggerLog,
+		FileName = ''' + @DataPath + 'BackloggerLog.ldf''
+	)'
+	EXEC(@SqlCode)
+
+	PRINT N'Database created in ' + @DataPath;
+GO
+
